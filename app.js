cb88a002fb8927d7428c4a4d621136395b13c7e4
Rafiul29 <rafiulislam4472@gmail.com>
2023-06-07 16:43:51 +0600

    global object and module system

---
+++ app.js
@@ -0,0 +1,9 @@
+//  IFFE
+const student=["rafi","pavel","badhon","rasu"]
+
+// console.log(module)
+// console.log(require.cache)
+
+module.exports=student
+
+//module.exports=student
---
+++ index.js
@@ -0,0 +1,12 @@
+const student=require('./app');
+
+// var b=4
+// console.log(global.b)
+
+
+// console.log(__dirname)
+// console.log(__filename )
+
+// console.log(student)
+
+console.log(require.extensions)
