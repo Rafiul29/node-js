0325d4bc450300be4c74ed7f8f1b94ef3db11a38
Rafiul29 <rafiulislam4472@gmail.com>
2023-11-20 21:10:06 +0600

    express

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express=require('express');
-
+const cors=require('cors')
 const app=express();
+
+app.use(cors());
+
+const users=[
+  {id:1,name:"rafiul",email:"rafi@gmial.com"},
+  {id:2,name:"pavel",email:"pavel@gmial.com"}
+]
 
 
 app.get("/",(req,res)=>{
-  res.send("hello word ")
+  res.send("Users Management Server running")
 })
-app.listen(3000,()=>{
-  console.log('listening on port 3000')
+app.get('/users',(req,res)=>{
+  res.json(users)
 })
+
+
+const port = process.env.PORT || 5000
+app.listen(port,()=>{
+  console.log(`Server is running on Port:${port}`)
+})
+
