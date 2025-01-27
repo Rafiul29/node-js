f712a92671b55872febbcd00a21815ebd26b4c50
Rafiul29 <rafiulislam4472@gmail.com>
2025-01-27 21:23:22 +0600

    update code

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-const express=require('express');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const fs = require("fs");
+const multer = require("multer");
+const mongoose = require("mongoose");
 
-const app=express();
+const app = express();
 
+app.use(cors());
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("hello word ");
+});
 
-app.get("/",(req,res)=>{
-  res.send("hello word ")
-})
-app.listen(3000,()=>{
-  console.log('listening on port 3000')
-})
+const PORT = 5000;
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/test_sb", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server running on http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.error("MongoDB connection error:", error));
